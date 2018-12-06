19daee22b09b91df1e170622806f4d322a8a928f
Jon Spriggs <jon@sprig.gs>
2018-12-06 14:16:57 +0000

    Based loosely on https://www.redpill-linpro.com/sysadvent/2017/12/24/ansible-system-updates.html

---
+++ filter_plugins/splitpart.py
@@ -0,0 +1,13 @@
+# From https://www.redpill-linpro.com/sysadvent/2017/12/24/ansible-system-updates.html
+def splitpart (value, index, char = ','):
+    if isinstance(value, (list, tuple)):
+        ret = []
+        for v in value:
+            ret.append(v.split(char)[index])
+        return ret
+    else:
+        return value.split(char)[index]
+
+class FilterModule(object):
+    def filters(self):
+        return {'splitpart': splitpart}
